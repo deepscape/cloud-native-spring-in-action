7526ea51b0e08bb96ce8febc36c12896df385382
deepscape <hdkim.scala@gmail.com>
2024-08-01 18:34:55 +0900

    Spring Data JDBC 적용 : p.195

--- src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -23,14 +23,14 @@ class BookValidationTests {
 
     @Test
     void whenAllFieldCorrectThenValidationSucceeds() {
-        var book = new Book("1234567890", "Title", "Author", 9.90);
+        var book = Book.of("1234567890", "Title", "Author", 9.90);
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).isEmpty();
     }
 
     @Test
     void whenIsbnDefinedButIncorrectThenValidationFails() {
-        var book = new Book("a234567890", "Title", "Author", 9.90);
+        var book = Book.of("a234567890", "Title", "Author", 9.90);
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
 
         // violations 집합의 크기가 1인지 확인하여 하나의 유효성 검사 오류가 있음을 확인
